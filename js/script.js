5f9de2096f0dbf33747bd57e09d2e7b94c3eed92
RiccardoBasso01 <riccardo.basso2001@gmail.com>
2023-06-07 16:49:25 +0200

    Griglia collegata al bottone

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 console.log('JS OK');
 
-// Recupero le celle della griglia
-const cell = document.querySelectorAll('.cell');
+// Recupero gli elementi dal DOM
+const grid = document.getElementById('grid'); // GRIGLIA
+const play = document.getElementById('play'); // BOTTONE
+const cell = document.querySelectorAll('.cell'); // CELLE
 console.log('Numero di celle: ', cell.length);
 
 // Assegno i numeri in sequenza a tutte le celle
 for(let i = 0; i < cell.length; i++){
     cell[i].innerText = i + 1;
 }
+
+// Al click del bottone
+play.addEventListener('click', function(){
+    // Faccio apparire la griglia
+    grid.classList.remove('d-none');
+
+    // Compaionp tutte le celle
+})
