aa0aa30c97039c9a2510aaf660bf317315486717
RiccardoBasso01 <riccardo.basso2001@gmail.com>
2023-06-07 17:00:59 +0200

    Creazione celle via JS

--- js/script.js
+++ js/script.js
@@ -3,16 +3,20 @@ console.log('JS OK');
 // Recupero gli elementi dal DOM
 const grid = document.getElementById('grid'); // GRIGLIA
 const play = document.getElementById('play'); // BOTTONE
+
+const numberOfCell = 100;
+
+// Genero le celle
+for (let i = 0; i < numberOfCell; i++) grid.innerHTML += `<div class="cell"></div>`;
+
 const cell = document.querySelectorAll('.cell'); // CELLE
 console.log('Numero di celle: ', cell.length);
 
 // Assegno i numeri in sequenza a tutte le celle
-for(let i = 0; i < cell.length; i++){
-    cell[i].innerText = i + 1;
-}
+for (let i = 0; i < cell.length; i++) cell[i].innerText = i + 1;
 
 // Al click del bottone
-play.addEventListener('click', function(){
+play.addEventListener('click', function () {
     // Faccio apparire la griglia
     grid.classList.remove('d-none');
 
