019f23ecbc6cecd82984c8b40091c2e81c7ee24c
RiccardoBasso01 <riccardo.basso2001@gmail.com>
2023-06-07 16:37:19 +0200

    Numerazione delle celle

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log('JS OK')
+console.log('JS OK');
+
+// Recupero le celle della griglia
+const cell = document.querySelectorAll('.cell');
+console.log('Numero di celle: ', cell.length);
+
+// Assegno i numeri in sequenza a tutte le celle
+for(let i = 0; i < cell.length; i++){
+    cell[i].innerText = i + 1;
+}
