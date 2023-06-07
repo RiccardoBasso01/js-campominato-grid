c5a291bab734804dc39963c54c742a04f8e414ba
RiccardoBasso01 <riccardo.basso2001@gmail.com>
2023-06-07 19:13:09 +0200

    Aggiunta bombe

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ const difficulty = document.getElementById('difficulty'); // DIFFICOLTÁ
 const bombNumber = document.getElementById('bomb-number'); // Numero di BOMBE
 // Numero di celle
 let numberOfCell;
+let numberOfBomb;
 
 // Al click del bottone gioca
 play.addEventListener('click', function () {
@@ -19,6 +20,7 @@ play.addEventListener('click', function () {
         grid.classList.remove('hard');
 
         numberOfCell = '64';
+        numberOfBomb = 10;
         bombNumber.innerText = '10 BOMBE';
 
     }else if(difficulty.value === 'medium'){
@@ -27,6 +29,7 @@ play.addEventListener('click', function () {
         grid.classList.remove('hard');
         
         numberOfCell = '81';
+        numberOfBomb = 20;
         bombNumber.innerText = '20 BOMBE';
         
     }else if(difficulty.value === 'hard'){
@@ -35,6 +38,7 @@ play.addEventListener('click', function () {
         grid.classList.add('hard');
         
         numberOfCell = '100';
+        numberOfBomb = 30;
         bombNumber.innerText = '30 BOMBE';
 
     }
@@ -53,10 +57,32 @@ play.addEventListener('click', function () {
     // Assegno i numeri in sequenza a tutte le celle
     for (let i = 0; i < cell.length; i++) {
         cell[i].innerText = i + 1;
-        // Al click sulle celle
+
+
+        //! Al click sulle celle
         cell[i].addEventListener('click', function () {
-            console.log('Cliccata cella numero: ', i);
             cell[i].classList.toggle('active');
+            const x = i + 1;// Numero della cella cliccata
+            console.log('Cliccata cella numero: ', x);
+
+            // Genera celle con le bombe
+            // Array con i numeri random
+            const bomb = [];
+            let randomNumber;
+            for(let i = 0; i < numberOfBomb; i++){
+                do{
+                    randomNumber = Math.floor((Math.random() * cell.length) + 1);
+                }while(bomb.includes(randomNumber) || randomNumber === x) // Estrae tot numeri tutti diversi tra loro e diversi dal numero della cella cliccata
+
+                bomb.push(randomNumber);
+            }
+            console.table(bomb);
+
+            for(let i = 0; i < bomb.length; i++){
+                console.log(bomb[i])
+                cell[bomb[i] - 1].classList.add('bomb');
+                cell[bomb[i] - 1].innerText = '';
+            }    
         })
     }
     
